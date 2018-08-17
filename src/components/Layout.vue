6cff4162ecc485eee25d296a18742b970fb116d6
baihua <baihua_2011@163.com>
2018-08-17 22:55:38 +0800

    基本框架

---
+++ src/components/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="app-head">
+      <div class="app-head-inner">
+          <img src="../assets/logo.png">
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="app-content">content</div>
+    <div class="app-foot">
+      <p>© 2018 大漠胡杨 MIT</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Layout'
+    }
+</script>
+
+<style>
+
+</style>
