dc7d2b8651ab1c1e68be4b1d97531c49c9928e4e
baihua <baihua_2011@163.com>
2018-08-18 20:46:29 +0800

    首页

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,7 +5,7 @@
           <img src="../assets/logo.png">
         <div class="head-nav">
           <ul class="nav-list">
-            <li>登录</li>
+            <li><router-link to="/hello">登录</router-link></li>
             <li class="nav-pile">|</li>
             <li>注册</li>
             <li class="nav-pile">|</li>
@@ -14,7 +14,9 @@
         </div>
       </div>
     </div>
-    <div class="app-content">content</div>
+    <div class="app-content">
+      <router-view/>
+    </div>
     <div class="app-foot">
       <p>© 2018 大漠胡杨 MIT</p>
     </div>
@@ -28,5 +30,98 @@
 </script>
 
 <style>
+  body {
+    background: #f0f2f5;
+    font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+    font-size: 14px;
+    color: #444;
+  }
+  .app-head {
+    background: #363636;
+    color: #b2b2b2;
+    height: 90px;
+    line-height: 90px;
+    width: 100%;
+  }
+  .app-head-inner {
+    width: 1200px;
+    margin: 0 auto;
+  }
+  .head-logo {
+    float: left;
+  }
+  .app-head-inner img {
+    width: 50px;
+    margin-top: 20px;
+  }
+  .head-nav {
+    float: right;
+  }
+  .head-nav ul {
+    overflow: hidden;
+  }
+  .head-nav li {
+    cursor: pointer;
+    float: left;
+  }
+  .nav-pile {
+    padding: 0 10px;
+  }
+  .app-foot {
+    text-align: center;
+    height: 80px;
+    width: 100%;
+    line-height: 80px;
+    background: #e3e4e8;
+    clear: both;
+    margin-top: 30px;
+  }
+  .container {
+    width: 1200px;
+    margin: 0 auto;
+  }
+  .hr {
+    height: 1px;
+    width: 100%;
+    background: #ddd;
+  }
+  .button {
+    background: #4fc08d;
+    color: #fff;
+    display: inline-block;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  .button:hover {
+    background: #4fc08d;
+  }
+  .g-form {
 
+  }
+  .g-form-line {
+    padding: 15px 0;
+  }
+  .g-form-label {
+    width: 100px;
+    font-size: 16px;
+    display: inline-block;
+  }
+  .g-form-input {
+    display: inline-block;
+  }
+  .g-form-input input {
+    height: 30px;
+    width: 200px;
+    line-height: 30px;
+    vertical-align: middle;
+    padding: 0 10px;
+    border: 1px solid #ccc;
+  }
+  .g-form-btn {
+    padding-left: 100px;
+  }
+  .g-form-error {
+    color: red;
+    padding-left: 15px;
+  }
 </style>
