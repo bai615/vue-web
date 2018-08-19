efdca73e7b09ff96868abb39cedd40e0c74d2f9d
baihua <baihua_2011@163.com>
2018-08-19 21:41:36 +0800

    登录、注册表单

---
+++ src/components/RegForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      this is a register form
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'RegForm'
+    }
+</script>
+
+<style scoped>
+
+</style>
