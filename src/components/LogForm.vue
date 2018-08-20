7647d5afde009270e78e334b29e7376d53d73eba
baihua <baihua_2011@163.com>
2018-08-20 21:23:00 +0800

    表单校验

--- src/components/LogForm.vue
+++ src/components/LogForm.vue
@@ -7,7 +7,7 @@
           <input type="text"
                  v-model="usernameModel" placeholder="请输入用户名">
         </div>
-        <!--<span class="g-form-error">{{ userErrors.errorText }}</span>-->
+        <span class="g-form-error">{{ userErrors.errorText }}</span>
       </div>
       <div class="g-form-line">
         <span class="g-form-label">密码：</span>
@@ -15,14 +15,14 @@
           <input type="password"
                  v-model="passwordModel" placeholder="请输入密码">
         </div>
-        <!--<span class="g-form-error">{{ passwordErrors.errorText }}</span>-->
+        <span class="g-form-error">{{ passwordErrors.errorText }}</span>
       </div>
       <div class="g-form-line">
         <div class="g-form-btn">
           <a class="button" @click="onLogin">登录</a>
         </div>
       </div>
-      <!--<p>{{ errorText }}</p>-->
+      <p>{{ errorText }}</p>
     </div>
   </div>
 </template>
@@ -33,12 +33,59 @@
     data () {
       return {
         usernameModel: '',
-        passwordModel: ''
+        passwordModel: '',
+        errorText: '',
+        userFlag: false,
+        passwordFlag: false
+      }
+    },
+    computed: {
+      userErrors () {
+        let errorText, status
+        if (!/@/g.test(this.usernameModel)) {
+          status = false
+          errorText = '用户名需包含@'
+        } else {
+          status = true
+          errorText = ''
+        }
+        if (!this.userFlag) {
+          errorText = ''
+          this.userFlag = true
+        }
+        return {
+          status,
+          errorText
+        }
+      },
+      passwordErrors () {
+        let errorText, status
+        if (!/^\w{6,8}$/g.test(this.passwordModel)) {
+          status = false
+          errorText = '密码6-8位'
+        } else {
+          status = true
+          errorText = ''
+        }
+        if (!this.passwordFlag) {
+          errorText = ''
+          this.passwordFlag = true
+        }
+        return {
+          status,
+          errorText
+        }
       }
     },
     methods: {
       onLogin () {
-        console.log(this.usernameModel, this.passwordModel)
+        if (!this.userErrors.status || !this.passwordErrors.status) {
+          this.errorText = '用户名或者密码格式错误'
+        } else {
+          this.errorText = ''
+          console.log('logging')
+        }
+        // console.log(this.usernameModel, this.passwordModel)
       }
     }
   }
